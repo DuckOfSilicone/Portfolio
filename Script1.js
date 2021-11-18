ec5f15fbe31eaa0187e2a2e59f7df7ba6ae0ad8e
Ethan Newell <jayfeather.een@gmail.com>
2021-11-18 17:20:46 -0600

    HTML jumpstart section 1

---
+++ Script1.js
@@ -0,0 +1,16 @@
+function test() {
+    // JavaScript source code
+    let y = 1;
+
+    let x = y;
+
+    console.log("x = " + x);
+    console.log("y = " + y);
+
+    y = 2;
+
+    console.log("x = " + x);
+}
+
+
+test();
